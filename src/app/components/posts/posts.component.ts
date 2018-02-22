9642e13e0ced530cffe7058dc9064b3b82b47815
Jose Ramirez <jsayram@Gmail.com>
2018-02-22 17:47:22 -0500

    Added DELETE request functionality for post - finished full CRUD functionality for post using Http client

--- src/app/components/posts/posts.component.ts
+++ src/app/components/posts/posts.component.ts
@@ -50,5 +50,17 @@ export class PostsComponent implements OnInit {
   	});
   }
 
+  removePost(post: Post){
+  	if(confirm('Are you Sure?')) {
+  		this.postService.removePost(post.id).subscribe(()=>{
+  			this.posts.forEach((cur, index)=>{
+		  		if(post.id === cur.id){
+		  			this.posts.splice(index, 1);
+		  		}
+		  	});
+  		})
+  	}
+  }
+
 
 } 
--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -27,5 +27,13 @@ export class PostService {
   	const url = `${this.postUrl}/${post.id}`;
   	return this.http.put<Post>(url, post, httpOptions);
   }
+
+  removePost(post: Post | number) : Observable<Post> {
+  	const id = typeof post === 'number' ? post : post.id;
+  	const url = `${this.postUrl}/${id}`; 
+
+  	return this.http.delete<Post>(url, httpOptions);
+
+  }
  
 }
